0afbd05110e9547ba4a35e2558af6b2dd1f6ccad
bbyshp <bbyshp@126.com>
2018-10-11 21:11:26 +0800

    no test

--- 001-spring-cloud-eureka/3-eureka-cluster/src/test/java/com/neo/SpringCloudEurekaApplicationTests.java
+++ 001-spring-cloud-eureka/3-eureka-cluster/src/test/java/com/neo/SpringCloudEurekaApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringCloudEurekaApplicationTests {
 
-	@Test
+	//@Test
 	public void contextLoads() {
 	}
 
